ba05ccacc60b62d86a40c12a02287bd7118edaf6
k40L1n <droidaarqam@gmail.com>
2020-09-13 15:53:02 +0530

    simple counter - 1st

---
+++ app.js
@@ -0,0 +1,28 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+
+        if (styles.contains('decrease')) {
+            count -= 1;
+        } else if (styles.contains('increase')) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red'
+        } else {
+            value.style.color = ''
+        }
+        
+        value.textContent = count;
+    })
+})
